319d8076af8047cfc9e6be64aabc7a6e25680ea1
Vladislav Otdelyanov <vlad_1995@mail.ru>
2024-02-14 19:52:32 +0300

    Add project files.

---
+++ AppData/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNP24_5_2.AppData
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using RNP24_5_2.AppData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RNP24_5_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<User> userList = new List<User>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            userList = CSVHelper.Read().ToList();
+
+            foreach (User user in userList)
+            {
+                UsersLv.Items.Add(user);
+            }
+        }
+
+        private void AddUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GoBackBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
